88773d7cb399c9791a52ff8a4272016c64ff1577
julien382 <julhermain@gmail.com>
2023-07-05 22:38:17 +0200

    first commit

---
+++ src/pages/Accueil/Accueil.jsx
@@ -0,0 +1,15 @@
+import './Accueil.scss'
+
+const Accueil = () => {
+
+
+    return (
+        <div>
+            
+        </div>
+
+
+    )
+}
+
+export default Accueil
---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import Accueil from "../pages/Accueil/Accueil";
+
+function App() {
+
+  return (
+    <div id="app">
+      <Accueil />
+
+    </div>
+  )
+}
+
+export default App
