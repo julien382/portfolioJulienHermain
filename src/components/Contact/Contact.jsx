4b9bcd13558639f556c72c13a6332b8e4a053a6e
julien382 <julhermain@gmail.com>
2023-07-06 00:44:34 +0200

    create contact

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+import './Contact.scss'
+
+const Contact = () => {
+
+
+    return (
+        <div>
+            <div className='cardContact'>
+                <h4>Contact</h4>
+                <div className='line'></div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Contact
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,8 @@
-import Accueil from "../pages/Accueil/Accueil";
 import '../styles/reset.scss'
 import '../styles/index.scss'
+
+import Accueil from "../pages/Accueil/Accueil";
+import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
   return (
     <div id="app">
       <Accueil />
+      <Contact />
       <Footer />
 
     </div>
