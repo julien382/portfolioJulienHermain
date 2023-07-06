8303f09335a2d9674988e11add61efa99fe05c65
julien382 <julhermain@gmail.com>
2023-07-07 00:54:45 +0200

    delete - text paragraphName

--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -7,7 +7,7 @@ const Accueil = () => {
         <div>
             <div className='paragraphName'>
                 <h2>Julien Hermain</h2>
-                <h1>Développeur Java-Script React</h1>
+                <h1>Développeur JavaScript React</h1>
             </div>
         </div>
 
