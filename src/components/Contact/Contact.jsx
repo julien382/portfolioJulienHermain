e5f35e72d878e695786ce1eb81537e5e0b4bf39b
julien382 <julhermain@gmail.com>
2023-07-07 00:00:28 +0200

    add link

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,8 @@
 import './Contact.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
 
@@ -9,6 +13,15 @@ const Contact = () => {
                 <h4>Contact</h4>
             </div>
             <div className='contactBottom'>
+                <Link to="https://github.com/julien382">
+                    <FontAwesomeIcon icon={faGithub} className='linkContact'/>
+                </Link>
+                <Link to="julhermain@gmail.com">
+                    <FontAwesomeIcon icon={faEnvelope} className='linkContact'/>
+                </Link>
+                <Link to="https://www.linkedin.com/in/julien-hermain/">
+                    <FontAwesomeIcon icon={faLinkedin} className='linkContact'/>
+                </Link>
 
             </div>
         </div>
