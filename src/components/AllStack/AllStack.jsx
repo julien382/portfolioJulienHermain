42dbfefa9cd05fb3c8331d2725b3a264af823f3b
julien382 <julhermain@gmail.com>
2023-07-10 21:50:46 +0200

    add button allStack

--- src/components/AllStack/AllStack.jsx
+++ src/components/AllStack/AllStack.jsx
@@ -1,24 +1,56 @@
 import './AllStack.scss'
 import logo from "../../assets/_edb21061-07ad-4058-a37e-31d41ef7d083.jpg";
+import React, { useState } from 'react';
 
 const Contact = () => {
 
+    const [showAllImages, setShowAllImages] = useState(false);
+
+    const imagesData = [
+        logo,
+        logo,
+        logo,
+        logo,
+        logo,
+        logo,
+        logo,
+        logo,
+        logo,
+        logo,
+        logo,
+        logo
+    ];
+
+    const visibleImages = showAllImages ? imagesData : imagesData.slice(0, 6);
+
+    const handleShowMore = () => {
+        setShowAllImages(true);
+    };
+
+    const handleShowLess = () => {
+        setShowAllImages(false);
+    };
 
     return (
         <div className='allStackContainer'>
-            <div className='allStack'>
-                <img className='logoStack' src={logo} alt="logo Stack" />
-                <img className='logoStack' src={logo} alt="logo Stack" />
-                <img className='logoStack' src={logo} alt="logo Stack" />
-                <img className='logoStack' src={logo} alt="logo Stack" />
-                <img className='logoStack' src={logo} alt="logo Stack" />
-                <img className='logoStack' src={logo} alt="logo Stack" />
-
+            <div className="allStack">
+                {visibleImages.map((image, index) => (
+                <img className='logoStack' key={index} src={image} alt="Image" />
+                ))}
             </div>
-            <p>V</p>
+            {!showAllImages && (
+                <button onClick={handleShowMore}>
+                <p>V</p>
+                </button>
+            )}
+            {showAllImages && (
+                <button onClick={handleShowLess}>
+                <p>V</p>
+                </button>
+            )}
         </div>
-
-    )
+  )
 }
+
 
 export default Contact
