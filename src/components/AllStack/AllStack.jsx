37e4b44330db5ba09054b4fa7119d2d955196510
julien382 <julhermain@gmail.com>
2023-07-11 01:00:08 +0200

    add logo stack

--- src/components/AllStack/AllStack.jsx
+++ src/components/AllStack/AllStack.jsx
@@ -1,5 +1,20 @@
 import './AllStack.scss'
-import logo from "../../assets/_edb21061-07ad-4058-a37e-31d41ef7d083.jpg";
+import sass from "../../assets/stack/sass.png";
+import html from "../../assets/stack/html.png";
+import css from "../../assets/stack/css.png";
+import js from "../../assets/stack/js.png";
+import react from "../../assets/stack/react.png";
+import viteJS from "../../assets/stack/viteJS.png";
+import figma from "../../assets/stack/figma.png";
+import nodeJS from "../../assets/stack/nodeJS.png";
+import redux from "../../assets/stack/redux.png";
+import github from "../../assets/stack/github.png";
+import git from "../../assets/stack/git.png";
+import npm from "../../assets/stack/npm.png";
+import cPlusPlus from "../../assets/stack/cPlusPlus.png";
+import csharp from "../../assets/stack/cSharp.png";
+import python from "../../assets/stack/python.png";
+
 import React, { useState } from 'react';
 
 const Contact = () => {
@@ -7,18 +22,21 @@ const Contact = () => {
     const [showAllImages, setShowAllImages] = useState(false);
 
     const imagesData = [
-        logo,
-        logo,
-        logo,
-        logo,
-        logo,
-        logo,
-        logo,
-        logo,
-        logo,
-        logo,
-        logo,
-        logo
+        js,
+        react,
+        viteJS,
+        html,
+        css,
+        sass,
+        nodeJS,
+        redux,
+        figma,
+        github,
+        npm,
+        git,
+        python,
+        cPlusPlus,
+        csharp,
     ];
 
     const visibleImages = showAllImages ? imagesData : imagesData.slice(0, 6);
@@ -35,7 +53,9 @@ const Contact = () => {
         <div className='allStackContainer'>
             <div className="allStack">
                 {visibleImages.map((image, index) => (
-                <img className='logoStack' key={index} src={image} alt="Image" />
+                <div className='logoStackContainer'>
+                    <img className='logoStack' key={index} src={image} alt="Image" />
+                </div>
                 ))}
             </div>
             {!showAllImages && (
