e731102ab8015c6df4cb289de47382f40a767004
julien382 <julhermain@gmail.com>
2023-07-11 23:31:14 +0200

    change svg arrow cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -68,14 +68,16 @@ const Cards = () => {
         </div>
         {!showAllCards ? (
           <div className='arrowContainer' onClick={handleShowMore}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='bi bi-arrow-down-circle arrow' viewBox="0 0 16 16">
-                  <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" viewBox="0 0 51 51" fill="none" className='arrow'>
+                <path d="M25.1025 2C12.348 2 2 12.348 2 25.1025C2 37.8571 12.348 48.2051 25.1025 48.2051C37.8571 48.2051 48.2051 37.8571 48.2051 25.1025C48.2051 12.348 37.8571 2 25.1025 2Z" stroke="black" stroke-width="3.85042" stroke-miterlimit="10"/>
+                <path d="M36.6538 20.2895L25.1025 31.8408L13.5513 20.2895" stroke="black" stroke-width="3.85042" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
           </div>
         ) : (
           <div className='arrowContainer' onClick={handleShowLess}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='bi bi-arrow-down-circle arrow arrowReturn' viewBox="0 0 16 16">
-                  <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" viewBox="0 0 51 51" fill="none" className='arrow arrowReturn'>
+                <path d="M25.1025 2C12.348 2 2 12.348 2 25.1025C2 37.8571 12.348 48.2051 25.1025 48.2051C37.8571 48.2051 48.2051 37.8571 48.2051 25.1025C48.2051 12.348 37.8571 2 25.1025 2Z" stroke="black" stroke-width="3.85042" stroke-miterlimit="10"/>
+                <path d="M36.6538 20.2895L25.1025 31.8408L13.5513 20.2895" stroke="black" stroke-width="3.85042" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
           </div>
         )}
@@ -84,3 +86,5 @@ const Cards = () => {
   };
   
   export default Cards;
+
+  
