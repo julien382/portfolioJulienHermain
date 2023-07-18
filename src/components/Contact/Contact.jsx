694b81bfe611fa157423b8d322032a3e28f63221
julien382 <julhermain@gmail.com>
2023-07-18 15:58:09 +0200

    contact center

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,20 +8,23 @@ const Contact = () => {
 
 
     return (
-        <div>
-            <div className='contactTop'>
-                <h4>Contact</h4>
-            </div>
-            <div className='contactBottom'>
-                <Link to="https://github.com/julien382">
-                    <FontAwesomeIcon icon={faGithub} className='linkContact'/>
-                </Link>
-                <Link to="mailto:julhermain@gmail.com">
-                    <FontAwesomeIcon icon={faEnvelope} className='linkContact'/>
-                </Link>
-                <Link to="https://www.linkedin.com/in/julien-hermain/">
-                    <FontAwesomeIcon icon={faLinkedin} className='linkContact'/>
-                </Link>
+        <div className='contactContainer'>
+            <div>
+                <div className='contactTop'>
+                    <h4>Contact</h4>
+                </div>
+                <div className='contactBottom'>
+                    <Link to="https://github.com/julien382">
+                        <FontAwesomeIcon icon={faGithub} className='linkContact'/>
+                    </Link>
+                    <Link to="mailto:julhermain@gmail.com">
+                        <FontAwesomeIcon icon={faEnvelope} className='linkContact'/>
+                    </Link>
+                    <Link to="https://www.linkedin.com/in/julien-hermain/">
+                        <FontAwesomeIcon icon={faLinkedin} className='linkContact'/>
+                    </Link>
+
+                </div>
 
             </div>
         </div>
