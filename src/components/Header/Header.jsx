5ec07e607c22d419ee0290203503624a5bbf49f5
julien382 <julhermain@gmail.com>
2024-04-16 22:15:19 +0200

    change border page and change cv for 2024

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import './Header.scss';
-import pdfFile from '../../data/CV_JULIEN_HERMAIN_2023_Openclassrooms.pdf';
+import pdfFile from '../../data/CV_JULIEN_HERMAIN_2024_Openclassrooms.pdf';
 
 const Header = () => {
   const openPdf = () => {
